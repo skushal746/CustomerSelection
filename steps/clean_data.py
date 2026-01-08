aec2bf1f00431e97a3de9bc0dcd6b4f0bb495f45
Kushal Sharma <skushal746@gmail.com>
2026-01-07 22:14:41 -0500

    Changed the return of clean data

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -1,16 +1,37 @@
 import logging
 import pandas as pd
 from zenml import step
+from src.data_cleaning import DataCleaning, DataDivideStrategy, DataPreProcessStrategy
 
 @step
-def clean_data(df: pd.DataFrame) -> None:
+def clean_data(df: pd.DataFrame) -> Tuple[
+    Annotated[pd.DataFrame, "x_train"],
+    Annotated[pd.DataFrame, "x_test"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.Series, "y_test"],
+]:
     """
-    Clean the data by removing duplicates and null values.
+    Data cleaning class which preprocesses the data and divides it into train and test data.
 
     Args:
-        df: Input DataFrame.
-
-    Returns:
-        None
+        data: pd.DataFrame
+    Returns
+        x_train: Training data
+        x_test: Testing data
+        y_train: Training labels
+        y_test: Testing labels
     """
-    pass
+
+    try:
+        process_strategy = DataPreProcessStrategy()
+        data_cleaning = DataCleaning(df, process_strategy)
+        processed_data = data_cleaning.handle_data()
+
+        divide_strategy = DataDicideStrategy()
+        data_cleaning = DataCleaning(processed_data, divide_strategy)
+        x_train, x_test, y_train, y_test = data_cleaning.handle_data()
+        logging.info("Data cleaning completed")
+        return x_train, x_test, y_train, y_test 
+    except Exception as e:
+        logging.error("Error in cleaning data: {}".format(e))
+        raise e
